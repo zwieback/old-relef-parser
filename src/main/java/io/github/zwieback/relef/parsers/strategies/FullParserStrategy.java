a18bda453ef071bf015a0ea6f28a0808b6f1f6cf
Anton Avdeev <zw13back@gmail.com>
2017-05-09 18:46:24 +0400

    RP-3: added FullParserStrategy to parse whole site

---
+++ src/main/java/io/github/zwieback/relef/parsers/strategies/FullParserStrategy.java
@@ -0,0 +1,122 @@
+package io.github.zwieback.relef.parsers.strategies;
+
+import io.github.zwieback.relef.entities.Brand;
+import io.github.zwieback.relef.entities.Catalog;
+import io.github.zwieback.relef.entities.CatalogLevel;
+import io.github.zwieback.relef.entities.Product;
+import io.github.zwieback.relef.parsers.CatalogParser;
+import io.github.zwieback.relef.parsers.CatalogsParser;
+import io.github.zwieback.relef.repositories.BrandRepository;
+import io.github.zwieback.relef.repositories.CatalogRepository;
+import io.github.zwieback.relef.repositories.ProductRepository;
+import io.github.zwieback.relef.services.CatalogLevelService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static io.github.zwieback.relef.parsers.strategies.ParserStrategy.FULL_STRATEGY;
+
+@Service(FULL_STRATEGY)
+public class FullParserStrategy extends ParserStrategy {
+
+    private static final Logger log = LogManager.getLogger(FullParserStrategy.class);
+
+    private final CatalogsParser catalogsParser;
+    private final CatalogParser catalogParser;
+    private final BrandRepository brandRepository;
+    private final CatalogRepository catalogRepository;
+    private final ProductRepository productRepository;
+    private final CatalogLevelService catalogLevelService;
+
+    @Value("${site.domain.catalogs}")
+    private String catalogsUrl;
+
+    public FullParserStrategy(CatalogsParser catalogsParser, CatalogParser catalogParser,
+                              BrandRepository brandRepository, CatalogRepository catalogRepository,
+                              ProductRepository productRepository, CatalogLevelService catalogLevelService) {
+        this.catalogsParser = catalogsParser;
+        this.catalogParser = catalogParser;
+        this.brandRepository = brandRepository;
+        this.catalogRepository = catalogRepository;
+        this.productRepository = productRepository;
+        this.catalogLevelService = catalogLevelService;
+    }
+
+    @Override
+    public void setEntityIds(String entityIds) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void parse() {
+        Document catalogsDoc = catalogsParser.parseUrl(catalogsUrl);
+        parseBrands(catalogsDoc);
+        parseTreeOfCatalogs(catalogsDoc);
+        parseCatalogsOfLastLevel(catalogsDoc);
+    }
+
+    private void parseBrands(Document catalogsDoc) {
+        List<Brand> brands = catalogsParser.parseBrands(catalogsDoc);
+        log.info(String.format("Found %d brands", brands.size()));
+        saveBrands(brands);
+    }
+
+    @Transactional
+    private void saveBrands(List<Brand> brands) {
+        brandRepository.save(brands);
+    }
+
+    private void parseTreeOfCatalogs(Document catalogsDoc) {
+        Set<CatalogLevel> levels = catalogLevelService.collectActiveCatalogLevels();
+        levels.forEach(level -> {
+            List<String> catalogUrls = catalogsParser.parseCatalogUrls(catalogsDoc, level);
+            log.info(String.format("Found %d catalogs of level %s", catalogUrls.size(), level));
+        });
+        List<Catalog> treeOfCatalogs = catalogsParser.parseTreeOfCatalogs(catalogsDoc, CatalogLevel.FIRST);
+        saveTreeOfCatalogs(treeOfCatalogs);
+    }
+
+    @Transactional
+    private void saveTreeOfCatalogs(List<Catalog> catalogs) {
+        catalogs.forEach(catalog -> {
+            if (!catalog.getChildren().isEmpty()) {
+                saveTreeOfCatalogs(catalog.getChildren());
+            }
+        });
+        catalogRepository.save(catalogs);
+    }
+
+    private void parseCatalogsOfLastLevel(Document catalogsDoc) {
+        CatalogLevel lastLevel = catalogLevelService.determineLastCatalogLevel();
+        List<Catalog> catalogs = catalogsParser.parseCatalogsOfLevel(catalogsDoc, lastLevel);
+        IntStream.range(0, catalogs.size())
+                .forEach(i -> {
+                    log.info(String.format("Parse catalog %d of %d", i, catalogs.size()));
+                    parseProductsOfCatalog(catalogs.get(i));
+                });
+    }
+
+    private void parseProductsOfCatalog(Catalog catalog) {
+        String url = catalog.getUrl();
+        List<Document> documents = catalogParser.parseUrl(url);
+        List<Product> products = documents.stream()
+                .map(document -> catalogParser.parseProducts(document, catalog.getId()))
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+        log.info(String.format("Found %d products in catalog %d", products.size(), catalog.getId()));
+        saveProducts(products);
+    }
+
+    @Transactional
+    private void saveProducts(List<Product> products) {
+        productRepository.save(products);
+    }
+}
