e7fe5744faec3d08fa91ea0ba05c71289885f5e4
Anton Avdeev <zw13back@gmail.com>
2017-05-09 18:44:05 +0400

    RP-3: added ProductParserStrategy to parse selected products

---
+++ src/main/java/io/github/zwieback/relef/parsers/strategies/ProductParserStrategy.java
@@ -0,0 +1,62 @@
+package io.github.zwieback.relef.parsers.strategies;
+
+import io.github.zwieback.relef.entities.Product;
+import io.github.zwieback.relef.parsers.ProductParser;
+import io.github.zwieback.relef.repositories.ProductRepository;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.jsoup.nodes.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.github.zwieback.relef.parsers.strategies.ParserStrategy.PRODUCT_STRATEGY;
+
+@Service(PRODUCT_STRATEGY)
+public class ProductParserStrategy extends ParserStrategy {
+
+    private static final Logger log = LogManager.getLogger(ProductParserStrategy.class);
+
+    private final ProductParser parser;
+    private final ProductRepository repository;
+
+    private List<Long> productIds;
+
+    @Autowired
+    public ProductParserStrategy(ProductParser parser, ProductRepository repository) {
+        this.parser = parser;
+        this.repository = repository;
+    }
+
+    @Override
+    public void setEntityIds(String entityIds) {
+        productIds = collectLongIds(entityIds);
+    }
+
+    @Override
+    public void parse() {
+        List<Long> existedIds = repository.findExistedIdsByIdIn(productIds);
+        if (productIds.size() != existedIds.size()) {
+            log.info("Only these product ids exist and will be parsed: " + existedIds);
+        }
+        List<Product> existedProducts = repository.findAll(existedIds);
+        List<Product> parsedProducts = existedProducts.stream().map(this::parseProduct).collect(Collectors.toList());
+        saveProducts(parsedProducts);
+    }
+
+    @NotNull
+    private Product parseProduct(Product existedProduct) {
+        String url = existedProduct.getUrl();
+        Document document = parser.parseUrl(url);
+        return parser.parseProduct(document, existedProduct.getCatalogId(), existedProduct.getId());
+    }
+
+    @Transactional
+    private void saveProducts(List<Product> products) {
+        repository.save(products);
+    }
+}
--- src/main/java/io/github/zwieback/relef/repositories/ProductRepository.java
+++ src/main/java/io/github/zwieback/relef/repositories/ProductRepository.java
@@ -2,8 +2,15 @@ package io.github.zwieback.relef.repositories;
 
 import io.github.zwieback.relef.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select id from Product where id in :ids")
+    List<Long> findExistedIdsByIdIn(@Param("ids") List<Long> ids);
 }
