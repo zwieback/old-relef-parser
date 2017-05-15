92117d27b4d1130a73193f1b5aefca08ee104312
Anton Avdeev <zw13back@gmail.com>
2017-05-15 17:16:39 +0400

    RP-7: added defaultString methods to StringService and tests for it

--- src/test/java/io/github/zwieback/relef/services/StringServiceTest.java
+++ src/test/java/io/github/zwieback/relef/services/StringServiceTest.java
@@ -1,12 +1,13 @@
 package io.github.zwieback.relef.services;
 
 import io.github.zwieback.relef.configs.ServiceConfig;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
@@ -14,8 +15,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 })
 public class StringServiceTest {
 
+    private static final String EMPTY_STRING = "";
     private static final String EXPECTED_STRING = "abc";
     private static final String DIRTY_STRING = "\u00A0" + EXPECTED_STRING;
+    private static final Long SOURCE_OBJECT = 100L;
 
     @SuppressWarnings("unused")
     @Autowired
@@ -24,12 +27,36 @@ public class StringServiceTest {
     @Test
     public void test_clean_should_return_cleaned_string() {
         String cleaned = stringService.clean(DIRTY_STRING);
-        Assert.assertEquals(EXPECTED_STRING, cleaned);
+        assertEquals(EXPECTED_STRING, cleaned);
     }
 
     @Test
     public void test_clean_should_return_same_string() {
         String cleaned = stringService.clean(EXPECTED_STRING);
-        Assert.assertEquals(EXPECTED_STRING, cleaned);
+        assertEquals(EXPECTED_STRING, cleaned);
+    }
+
+    @Test
+    public void test_defaultString_should_return_same_string() {
+        String result = stringService.defaultString(EXPECTED_STRING);
+        assertEquals(EXPECTED_STRING, result);
+    }
+
+    @Test
+    public void test_defaultString_should_return_empty_string() {
+        String result = stringService.defaultString(null);
+        assertEquals(EMPTY_STRING, result);
+    }
+
+    @Test
+    public void test_defaultString_object_should_return_string_representation_of_object() {
+        String result = stringService.defaultString(SOURCE_OBJECT);
+        assertEquals(SOURCE_OBJECT.toString(), result);
+    }
+
+    @Test
+    public void test_defaultString_object_should_return_empty_string() {
+        String result = stringService.defaultString((Long) null);
+        assertEquals(EMPTY_STRING, result);
     }
 }
