1a1904b01f86f8072ad577ab285933124fe0ae5f
Anton Avdeev <zw13back@gmail.com>
2017-05-23 15:38:19 +0400

    RP-11: added factory to RestTemplate for saving state (cookies and etc)

--- src/main/java/io/github/zwieback/relef/configs/WebConfig.java
+++ src/main/java/io/github/zwieback/relef/configs/WebConfig.java
@@ -1,8 +1,12 @@
 package io.github.zwieback.relef.configs;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,6 +15,8 @@ public class WebConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        ClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(factory);
     }
 }
