bea79073c445fc9ef4f95818346ca7b578b156b7
Anton Avdeev <zw13back@gmail.com>
2017-05-26 11:06:09 +0400

    RP-15: added tests for ProductPriceService

---
+++ src/test/java/io/github/zwieback/relef/web/services/ProductPriceServiceTest.java
@@ -0,0 +1,69 @@
+package io.github.zwieback.relef.web.services;
+
+import io.github.zwieback.relef.configs.WebConfigForTest;
+import io.github.zwieback.relef.entities.Product;
+import io.github.zwieback.relef.entities.dto.product.prices.ProductPricesDto;
+import io.github.zwieback.relef.utils.json.AbstractDeserializerTest;
+import io.github.zwieback.relef.web.rest.services.RestService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+        WebConfigForTest.class
+})
+public class ProductPriceServiceTest extends AbstractDeserializerTest {
+
+    @SuppressWarnings("unused")
+    @Autowired
+    private ProductPriceService productPriceService;
+
+    @SuppressWarnings("unused")
+    @Autowired
+    private RestService restService;
+
+    @Test
+    public void test_getPrices_should_return_prices_for_zero_products() throws IOException {
+        setProductPricesDto(readValue("classpath:json/product/prices/no_product.json", ProductPricesDto.class));
+        ProductPricesDto productPricesDto = productPriceService.getPrices(buildProductListStub(0));
+        assertEquals(0, productPricesDto.getProductMap().size());
+    }
+
+    @Test
+    public void test_getPrices_should_return_prices_for_one_product() throws IOException {
+        setProductPricesDto(readValue("classpath:json/product/prices/product_34259.json", ProductPricesDto.class));
+        ProductPricesDto productPricesDto = productPriceService.getPrices(buildProductListStub(1));
+        assertEquals(1, productPricesDto.getProductMap().size());
+    }
+
+    @Test
+    public void test_getPrices_should_return_prices_for_ten_product() throws IOException {
+        setProductPricesDto(readValue("classpath:json/product/prices/catalog_68526.json", ProductPricesDto.class));
+        ProductPricesDto productPricesDto = productPriceService.getPrices(buildProductListStub(10));
+        assertEquals(10, productPricesDto.getProductMap().size());
+    }
+
+    private void setProductPricesDto(ProductPricesDto productPricesDto) {
+        when(restService.post(anyString(), anyObject(), anyObject())).thenReturn(productPricesDto);
+    }
+
+    private static List<Product> buildProductListStub(int amount) {
+        return IntStream.range(0, amount)
+                .mapToObj(i -> new Product().setId(0L).setXmlId(UUID.randomUUID()).setDataType("basket"))
+                .collect(Collectors.toList());
+    }
+}
