bc05871a45f704d1467c8cd29f8d04a03d63e0fa
Anton Avdeev <zw13back@gmail.com>
2017-05-29 22:35:50 +0400

    RP-21: added ability to download photos of products

---
+++ src/main/java/io/github/zwieback/relef/downloaders/Downloader.java
@@ -0,0 +1,126 @@
+package io.github.zwieback.relef.downloaders;
+
+import io.github.zwieback.relef.exceptions.ExceededErrorsCountException;
+import io.github.zwieback.relef.services.FileService;
+import io.github.zwieback.relef.web.rest.services.RestService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public abstract class Downloader<T> {
+
+    private static final Logger log = LogManager.getLogger(Downloader.class);
+    private static final int ENTITY_ON_PAGE_COUNT = 100;
+
+    private final RestService restService;
+    private final FileService fileService;
+
+    @Value("${max.errors.number:10}")
+    private int maxErrorsNumber;
+
+    @Value("${download.path}")
+    private String downloadPath;
+
+    @Value("${download.skip.downloaded}")
+    private boolean skipDownloaded;
+
+    @Autowired
+    public Downloader(RestService restService, FileService fileService) {
+        this.restService = restService;
+        this.fileService = fileService;
+    }
+
+    /**
+     * Download all entities page by page.
+     */
+    public void download() {
+        Page<T> page = findAll(buildPageRequest());
+        log.info(String.format("Total elements %d on total pages %d", page.getTotalElements(), page.getTotalPages()));
+        downloadAllPages(page);
+    }
+
+    private void downloadAllPages(Page<T> page) {
+        while (!page.isLast()) {
+            downloadPage(page);
+            Pageable pageable = page.nextPageable();
+            page = findAll(pageable);
+        }
+        downloadPage(page);
+    }
+
+    private void downloadPage(Page<T> page) {
+        log.info(String.format("Download page %d of %d with %d entities", page.getNumber() + 1, page.getTotalPages(),
+                page.getNumberOfElements()));
+        downloadEntities(page.getContent());
+    }
+
+    private void downloadEntities(List<T> entities) {
+        int entitySize = entities.size();
+        AtomicInteger i = new AtomicInteger();
+        AtomicInteger errorCount = new AtomicInteger();
+        entities.forEach(entity -> {
+            String url = getEntityUrl(entity);
+            log.debug(String.format("Download entity %d of %d (%s)", i.incrementAndGet(), entitySize, url));
+            try {
+                downloadEntity(entity);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                if (errorCount.incrementAndGet() > maxErrorsNumber) {
+                    throw new ExceededErrorsCountException();
+                }
+            }
+        });
+    }
+
+    boolean downloadEntity(T entity) {
+        String url = getEntityUrl(entity);
+        String fileName = getFileName(entity);
+        if (StringUtils.isEmpty(url) || StringUtils.isEmpty(fileName)) {
+            return false;
+        }
+        String fileNameToSave = buildFileName(fileName);
+        if (skipDownloaded && fileService.exists(fileNameToSave)) {
+            return true;
+        }
+        byte[] bytes = downloadContentAsBytes(url);
+        fileService.writeBytes(bytes, fileNameToSave);
+        return true;
+    }
+
+    private byte[] downloadContentAsBytes(@NotNull String url) {
+        HttpHeaders httpHeaders = restService.buildEmptyHeaders();
+        HttpEntity<?> httpEntity = restService.buildEntity(null, httpHeaders);
+        return restService.get(url, httpEntity, byte[].class);
+    }
+
+    abstract Page<T> findAll(Pageable pageable);
+
+    @Nullable
+    abstract String getEntityUrl(T entity);
+
+    @Nullable
+    abstract String getFileName(T entity);
+
+    @NotNull
+    private String buildFileName(String relativeFileName) {
+        return downloadPath + File.separator + relativeFileName;
+    }
+
+    @NotNull
+    private static Pageable buildPageRequest() {
+        return new PageRequest(0, ENTITY_ON_PAGE_COUNT);
+    }
+}
---
+++ src/main/java/io/github/zwieback/relef/downloaders/ProductImageDownloader.java
@@ -0,0 +1,110 @@
+package io.github.zwieback.relef.downloaders;
+
+import com.google.common.io.Files;
+import io.github.zwieback.relef.downloaders.exporters.NameExporter;
+import io.github.zwieback.relef.entities.Product;
+import io.github.zwieback.relef.repositories.ProductRepository;
+import io.github.zwieback.relef.services.FileService;
+import io.github.zwieback.relef.services.StringService;
+import io.github.zwieback.relef.services.TransliterateService;
+import io.github.zwieback.relef.web.rest.services.RestService;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class ProductImageDownloader extends Downloader<Product> {
+
+    private final StringService stringService;
+    private final TransliterateService transliterateService;
+    private final ProductRepository productRepository;
+    private final NameExporter nameExporter;
+
+    private final Map<String, String> names;
+
+    @Value("${site.domain}")
+    private String domainUrl;
+
+    @Autowired
+    public ProductImageDownloader(RestService restService,
+                                  FileService fileService,
+                                  StringService stringService,
+                                  TransliterateService transliterateService,
+                                  ProductRepository productRepository,
+                                  NameExporter nameExporter) {
+        super(restService, fileService);
+        this.stringService = stringService;
+        this.transliterateService = transliterateService;
+        this.productRepository = productRepository;
+        this.nameExporter = nameExporter;
+
+        this.names = new HashMap<>();
+    }
+
+    @Override
+    public void download() {
+        super.download();
+        nameExporter.export(names);
+    }
+
+    @Override
+    boolean downloadEntity(Product entity) {
+        boolean downloaded = super.downloadEntity(entity);
+        if (!StringUtils.isEmpty(entity.getName())) {
+            if (downloaded) {
+                names.put(getProcessedName(entity.getName()), entity.getName());
+            } else {
+                names.remove(entity.getName());
+            }
+        }
+        return downloaded;
+    }
+
+    @Override
+    Page<Product> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Nullable
+    @Override
+    String getEntityUrl(Product entity) {
+        if (StringUtils.isEmpty(entity.getPhotoUrl()) || domainUrl.equals(entity.getPhotoUrl())) {
+            return null;
+        }
+        return entity.getPhotoUrl();
+    }
+
+    @Nullable
+    @Override
+    String getFileName(Product entity) {
+        if (StringUtils.isEmpty(entity.getName())) {
+            return null;
+        }
+        String extension = "";
+        if (!StringUtils.isEmpty(entity.getPhotoUrl())) {
+            extension = Files.getFileExtension(entity.getPhotoUrl());
+            extension = StringUtils.isEmpty(extension) ? "" : "." + extension;
+        }
+        return getProcessedName(entity.getName()) + extension;
+    }
+
+    /**
+     * Transliterate and replace special characters in source string.
+     *
+     * @param name source string
+     * @return processed string
+     */
+    @NotNull
+    private String getProcessedName(@NotNull String name) {
+        String transliterated = transliterateService.transliterate(name);
+        return stringService.replaceSpecialCharsByUnderscore(transliterated);
+    }
+}
